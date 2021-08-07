3932a4c0e88f152a9825a2dcb1a537a0b90929b2
Kamenev Nikolay <79969997909@mail.ru>
2021-08-07 21:11:11 +0300

    added styles for header

--- webpack.config.js
+++ webpack.config.js
@@ -1,34 +1,39 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
-const CopyPlugin = require("copy-webpack-plugin");
-const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const CopyPlugin = require('copy-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const ESLintPlugin = require('eslint-webpack-plugin');
 
 module.exports = (env, argv) => {
-  const isProd = argv.mode === 'production'
-  const isDev = !isProd
+  const isProd = argv.mode === 'production';
+  const isDev = !isProd;
 
-  const filename = ext => isProd ? `[name].[contenthash].bundle.${ext}` : `[name].bundle.${ext}`
+  const filename = (ext) => isProd ? `[name].[contenthash].bundle.${ext}` : `[name].bundle.${ext}`;
 
   const plugins = () => {
     const base = [
       new HtmlWebpackPlugin({
-        template: './index.html'
+        template: './index.html',
       }),
       new CopyPlugin({
         patterns: [
-          {from: path.resolve(__dirname, 'src', 'favicon.ico'), to: path.resolve(__dirname, 'dist')},
+          {
+            from: path.resolve(__dirname, 'src', 'favicon.ico'),
+            to: path.resolve(__dirname, 'dist'),
+          },
         ],
       }),
       new MiniCssExtractPlugin({
-        filename: filename('css')
-      })
-    ]
+        filename: filename('css'),
+      }),
+    ];
 
-    if (isDev) base.push(new ESLintPlugin(options))
+    if (isDev) {
+      base.push(new ESLintPlugin());
+    }
 
     return base;
-  }
+  };
 
   return {
     target: 'web',
@@ -37,8 +42,8 @@ module.exports = (env, argv) => {
       main: [
         'core-js/stable',
         'regenerator-runtime/runtime',
-        './index.ts'
-      ]
+        './index.ts',
+      ],
     },
     output: {
       path: path.resolve(__dirname, 'dist'),
@@ -48,27 +53,26 @@ module.exports = (env, argv) => {
     resolve: {
       extensions: ['.js', '.ts'],
       alias: {
-        "@": path.resolve(__dirname, 'src'),
-        "@core": path.resolve(__dirname, 'src', 'core')
-      }
+        '@': path.resolve(__dirname, 'src'),
+        '@core': path.resolve(__dirname, 'src', 'core'),
+      },
     },
-    plugins,
     devServer: {
       contentBase: path.join(__dirname, 'dist'),
       compress: true,
       port: 3000,
-      open: true,
-      hot: true
+      hot: true,
     },
     devtool: isDev && 'source-map',
+    plugins: plugins(),
     module: {
       rules: [
         {
           test: /\.s[ac]ss$/i,
           use: [
             MiniCssExtractPlugin.loader,
-            "css-loader",
-            "sass-loader",
+            'css-loader',
+            'sass-loader',
           ],
         },
         {
@@ -77,14 +81,14 @@ module.exports = (env, argv) => {
           use: {
             loader: 'babel-loader',
             options: {
-              presets: ['@babel/preset-env']
-            }
-          }
-        }
+              presets: ['@babel/preset-env'],
+            },
+          },
+        },
       ],
     },
-  }
-}
+  };
+};
 
 
 
