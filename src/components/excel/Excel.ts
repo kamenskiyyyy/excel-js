f2f23c4719dd08f7107385ce07481c08add40ca2
Kamenev Nikolay <79969997909@mail.ru>
2021-09-20 18:33:08 +0300

    feat: component template output

--- src/components/excel/Excel.ts
+++
@@ -1,9 +0,0 @@
-export class Excel {
-  private $el: HTMLElement;
-  private components: Array<any> = [];
-
-  constructor(selector, options) {
-    this.$el = document.querySelector(selector)
-    this.components = options.components
-  }
-}
